8e84cfa0fe1b62993dcda03aa77e0af0c318a697
sascha horn <saschahorn83@gmail.com>
2024-02-17 15:12:50 +0100

    add test getAllEmployees

--- backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
+++ backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
@@ -1,13 +1,28 @@
 package saho.backend.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class EmployeeControllerTest {
 
+    private final String BASE_URL = "/api";
+
+    @Autowired
+    private MockMvc mvc;
     @Test
-    void getAllEmployees() {
+    void getAllEmployees_shouldReturnEmptyList_WhenCalledInitially() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/employees"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("[]"));
     }
 
     @Test
