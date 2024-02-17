8c983745375f5cb93305397f91f0710b89f2c15c
sascha horn <saschahorn83@gmail.com>
2024-02-17 17:01:24 +0100

    add EmployeeServiceTest folder

---
+++ backend/src/test/java/saho/backend/service/EmployeeServiceTest.java
@@ -0,0 +1,28 @@
+package saho.backend.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeServiceTest {
+
+    @Test
+    void getAllEmployees_() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void saveEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+}
