14a7f1bc54083b10b40ca5f95a9797159b0e6562
sascha horn <saschahorn83@gmail.com>
2024-02-17 14:38:34 +0100

    add test folder

---
+++ backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
@@ -0,0 +1,28 @@
+package saho.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeControllerTest {
+
+    @Test
+    void getAllEmployees() {
+    }
+
+    @Test
+    void getEmployeeById() {
+    }
+
+    @Test
+    void saveEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void updateEmployee() {
+    }
+}
