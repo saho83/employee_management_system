a822a0f5b01687e0b2faca07b4e712feae86e5c6
sascha horn <saschahorn83@gmail.com>
2024-02-17 15:48:43 +0100

    add test getEmployeeById

--- backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
+++ backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
@@ -1,12 +1,16 @@
 package saho.backend.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import saho.backend.model.Employee;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,6 +22,10 @@ class EmployeeControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @Test
     void getAllEmployees_shouldReturnEmptyList_WhenCalledInitially() throws Exception {
         mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/employees"))
@@ -26,7 +34,30 @@ class EmployeeControllerTest {
     }
 
     @Test
-    void getEmployeeById() {
+    void getEmployeeById_shouldReturnEmployee1_WhenCalledWith1() throws Exception {
+        Employee employee = new Employee("1", "firstName", "lastName", "test@test.de");
+        String employeeAsJSON = objectMapper.writeValueAsString(employee);
+
+       MvcResult result = mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(employeeAsJSON))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+       Employee savedEmployee = objectMapper.readValue(result.getResponse().getContentAsString(), Employee.class);
+
+
+       mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/employees/1"))
+               .andExpect(MockMvcResultMatchers.status().isOk())
+               .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "id": "1",
+                            "firstName": "firstName",
+                            "lastName": "lastName",
+                            "emailId": "test@test.de"
+                        }
+                """));
+
     }
 
     @Test
