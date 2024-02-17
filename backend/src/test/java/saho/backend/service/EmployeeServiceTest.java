92c5d60410a2e3f8ca6bb169b6348cfea21e77bb
sascha horn <saschahorn83@gmail.com>
2024-02-17 17:51:37 +0100

    add test getAllEmployees

--- backend/src/test/java/saho/backend/service/EmployeeServiceTest.java
+++ backend/src/test/java/saho/backend/service/EmployeeServiceTest.java
@@ -1,13 +1,33 @@
 package saho.backend.service;
 
 import org.junit.jupiter.api.Test;
+import saho.backend.model.Employee;
+import saho.backend.repo.EmployeeRepo;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EmployeeServiceTest {
 
+    EmployeeRepo employeeRepo = mock(EmployeeRepo.class);
+    EmployeeService employeeService = new EmployeeService(employeeRepo);
+
     @Test
-    void getAllEmployees_() {
+    void getAllEmployees_whenCalled_thenReturnsAllEmployees() {
+        //GIVEN
+        when(employeeRepo.findAll()).thenReturn(List.of(new Employee("1", "firstName", "lastName", "test@test.de")));
+
+        //WHEN
+        List<Employee> actual = employeeService.getAllEmployees();
+
+        //THEN
+        List<Employee> expected = List.of(new Employee("1", "firstName", "lastName", "test@test.de"));
+        verify(employeeRepo).findAll();
+        assertEquals(expected, actual);
     }
 
     @Test
