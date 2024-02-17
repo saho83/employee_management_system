ec5ed3f7fcc3befdc1cb248b34f5080e25f9c5db
sascha horn <saschahorn83@gmail.com>
2024-02-17 16:18:25 +0100

    add test saveEmployee, updateEmployee

--- backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
+++ backend/src/test/java/saho/backend/controller/EmployeeControllerTest.java
@@ -2,6 +2,7 @@ package saho.backend.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import saho.backend.model.Employee;
+import saho.backend.repo.EmployeeRepo;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -25,6 +27,9 @@ class EmployeeControllerTest {
 
     @Autowired
     private ObjectMapper objectMapper;
+
+    @Autowired
+    private EmployeeRepo employeeRepo;
 
     @Test
     void getAllEmployees_shouldReturnEmptyList_WhenCalledInitially() throws Exception {
@@ -61,14 +66,42 @@ class EmployeeControllerTest {
     }
 
     @Test
-    void saveEmployee() {
+    void saveEmployee_shouldReturnSavedEmployee() throws Exception {
+        Employee employee = new Employee("1", "firstName", "lastName", "test@test.de");
+        String employeeAsJSON = objectMapper.writeValueAsString(employee);
+
+        mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeAsJSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 
-    @Test
-    void deleteEmployee() {
-    }
+
 
     @Test
-    void updateEmployee() {
+    void updateEmployee_shouldReturnUpdatedEmployee() throws Exception {
+        Employee employee = new Employee("1", "firstName", "lastName", "test@test.de");
+        String employeeAsJSON = objectMapper.writeValueAsString(employee);
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders.post(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(employeeAsJSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andReturn();
+
+        Employee savedEmployee = objectMapper.readValue(result.getResponse().getContentAsString(), Employee.class);
+
+
+        mvc.perform(MockMvcRequestBuilders.get(BASE_URL + "/employees/1"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                        {
+                            "id": "1",
+                            "firstName": "firstName",
+                            "lastName": "lastName",
+                            "emailId": "test@test.de"
+                        }
+                """));
     }
 }
